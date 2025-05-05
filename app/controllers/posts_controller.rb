64a56932fd43c8b0c394417b4ff5e46bc38e73c2
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-05-05 16:58:16 +0900

    入力した電話番号を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,6 +58,6 @@ class PostsController < ApplicationController
 
     private
     def post_params #ストロングパラメータ
-        params.require(:post).permit(:title, :content) #パラメーターのキー
+        params.require(:post).permit(:title, :content, :phoneNumber)  #パラメーターのキー
     end
 end
