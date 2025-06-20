9278d0ca9b23e957b7880595a8a45483a17350ca
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-06-20 21:38:03 +0900

    通知メール送信本番

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,7 @@ Rails.application.configure do
     end
   end  
 
-  host = 'sample-sns-project2.onrender.com'
-  config.action_mailer.default_url_options = { protocol: 'https', host: host } 
+  config.action_mailer.default_url_options = { protocol: 'https', host:'sample-sns-project2.onrender.com' } 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
